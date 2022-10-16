3c9be549d2e541e6451ab8cb0ee4c75bdaa1973d
SerhiiKutovyi <wagenrex@gmail.com>
2022-10-17 01:39:06 +0300

    HW-09_JS 1-end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-console.log('hello!');
+const buttonStart = document.querySelector('button[data-start="start"]');
+console.log(buttonStart);
+const buttonStop = document.querySelector('button[data-stop="stop"]');
+console.log(buttonStop);
+
+let timerId = null;
+let isActive = false;
+
+buttonStart.addEventListener('click', () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  console.log(buttonStart);
+
+  timerId = setInterval(() => {
+    randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  isActive = false;
+});
