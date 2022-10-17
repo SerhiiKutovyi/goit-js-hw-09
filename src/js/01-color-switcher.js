3a1ed03bcadff1743af0a2cb7a7b57b2a12346a2
SerhiiKutovyi <wagenrex@gmail.com>
2022-10-17 15:59:25 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,7 +11,6 @@ buttonStart.addEventListener('click', () => {
     return;
   }
   isActive = true;
-  console.log(buttonStart);
 
   timerId = setInterval(() => {
     randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
