992bb31de6889b2a728948292ec10bfb2febfec7
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2022-10-20 23:13:30 +0000

    Deploying to gh-pages from @ SerhiiKutovyi/goit-js-hw-09@cd096118ef0fe53be295ecf1da03443570c1218f 🚀

--- 01-color-switcher.a94388e3.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};let o=null,e=!1;t.start.addEventListener("click",(()=>{e||(e=!0,o=setInterval((()=>{randomColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,document.body.style.backgroundColor=randomColor}),1e3))})),t.stop.addEventListener("click",(()=>{clearInterval(o),e=!1}));
-//# sourceMappingURL=01-color-switcher.a94388e3.js.map
